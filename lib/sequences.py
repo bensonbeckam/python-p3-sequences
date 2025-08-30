9baae00c578e6af28e55f086596d60f586725aec
Beckham <bensonbeckham98@gmail.com>
2025-08-30 14:29:49 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n == 0:
+        print([])
+        return
+    
+    fib = [0, 1]
+    
+    if n == 1:
+        print([0])
+        return
+    
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    
+    print(fib[:n])
